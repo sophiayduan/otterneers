c49e0bd3cdb1a7550b77f0141a2183946744525f
Zoey Chen <zixiu.zoey.chen@gmail.com>
2026-03-03 16:14:02 -0500

    made typescipt and javascript files, not don't edit the js file directly, instead it is made using tsc

---
+++ easter-eggs.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+var string = "hello world";
+console.log(string);
+var lutriText = "Companion — Each nonland card in your starting deck has a different name. (If this card is your chosen companion, you may put it into your hand from outside the game for {3} as a sorcery.)\nFlash\nWhen Lutri enters, if you cast it, copy target instant or sorcery spell you control. You may choose new targets for the copy.";
---
+++ easter-eggs.ts
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+let string: string = "hello world";
+
+console.log(string);
+
+const lutriText: string = "Companion — Each nonland card in your starting deck has a different name. (If this card is your chosen companion, you may put it into your hand from outside the game for {3} as a sorcery.)\nFlash\nWhen Lutri enters, if you cast it, copy target instant or sorcery spell you control. You may choose new targets for the copy.";
