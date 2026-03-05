2c2125b7078df79acdcdeee2cb87545d363c5384
sophiayduan <sophiayduan@gmail.com>
2026-03-05 09:58:12 -0500

    add lenis and gsap

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # fire-project
 
 [figma](https://www.figma.com/design/13PkBfyHL2sQdROXN9B9Jl/OTTERNEERS?node-id=2002-2&t=Kj2oeaWoDjfAx3X3-1) 
+
+# Development 
+
+Run 
+``` npm install ```
+
+Open it with live server
---
+++ script.js
@@ -0,0 +1,12 @@
+import Lenis from 'lenis'
+import { gsap } from "gsap";
+
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
